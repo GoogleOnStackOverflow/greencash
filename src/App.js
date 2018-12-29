50b1bfdbd33a63ecd7b3c3689878bf44779a5a21
GoogleOnStackOverflow <b01901111@ntu.edu.tw>
2018-12-29 13:20:00 +0800

    Edit format of qr information

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
     let qr_str = JSON.stringify({
       info: machineInfo,
-      content: {
+      recycle: {
         bottle: this.state.bottle,
         can: this.state.can,
       },
